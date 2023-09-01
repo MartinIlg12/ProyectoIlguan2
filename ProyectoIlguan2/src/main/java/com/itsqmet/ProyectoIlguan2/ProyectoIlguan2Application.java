780ec901d4d94e7a2e5b1f65a6b514fa33c6f12a
martinilguan12221@gmail.com <MartinIlg12@users.noreply.github.com>
2023-09-01 14:20:39 -0500

    Nuevo

---
+++ ProyectoIlguan2/src/main/java/com/itsqmet/ProyectoIlguan2/ProyectoIlguan2Application.java
@@ -0,0 +1,13 @@
+package com.itsqmet.ProyectoIlguan2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoIlguan2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoIlguan2Application.class, args);
+	}
+
+}
---
+++ ProyectoIlguan2/src/test/java/com/itsqmet/ProyectoIlguan2/ProyectoIlguan2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.itsqmet.ProyectoIlguan2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoIlguan2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
